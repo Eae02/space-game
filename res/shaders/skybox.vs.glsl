0630fc958d12fd430618714836f439e112c713e5
Erik Amirell Eklöf <erik.amirell.eklof@gmail.com>
2020-10-12 22:42:41 +0200

    fix skybox depth

--- res/shaders/skybox.vs.glsl
+++ res/shaders/skybox.vs.glsl
@@ -8,7 +8,7 @@ out vec3 eyeVector_v;
 #include rendersettings.glh
 
 void main() {
-	gl_Position = vec4(positions[gl_VertexID], 1, 1);
+	gl_Position = vec4(positions[gl_VertexID], 0.99999, 1);
 	vec4 farFrustumCoord = rs.vpMatrixInv * vec4(positions[gl_VertexID], 1, 1);
 	vec4 nearFrustumCoord = rs.vpMatrixInv * vec4(positions[gl_VertexID], -1, 1);
 	eyeVector_v = farFrustumCoord.xyz / farFrustumCoord.w - nearFrustumCoord.xyz / nearFrustumCoord.w;
